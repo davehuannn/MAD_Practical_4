39329e311903c22d89a3d847cf972d662c116ceb
Dave Huan <davehuan123@gmail.com>
2019-11-05 15:42:40 +0800

    Stop at number 8

--- Telegramme/ViewController.swift
+++ Telegramme/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usrNameFld: UITextField!
+    @IBOutlet weak var pwdFld: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginBtn(_ sender: Any) {
+        if true { // this is to check if the username and password is correct
+            let storyboard = UIStoryboard(name: "Content", bundle: nil)
+            let vc = storyboard.instantiateViewController(identifier: "Content") as UIViewController
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: true, completion: nil)
+        }
+    }
+    
 }
 
