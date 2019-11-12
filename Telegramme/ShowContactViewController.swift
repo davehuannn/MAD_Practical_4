58c0f401acac1d338d25b074870d40388fd6a0cf
Dave Huan <davehuan123@gmail.com>
2019-11-12 14:10:57 +0800

    Practical 4 Finished

---
+++ Telegramme/ShowContactViewController.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+import UIKit
+
+class ShowContactViewController :  UITableViewController {
+    
+    var appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return appDelegate.contactList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        let contact = appDelegate.contactList[indexPath.row]
+        cell.textLabel!.text = "\(contact.firstName) \(contact.lastName)"
+        cell.detailTextLabel!.text = "\(contact.mobileNo)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            appDelegate.contactList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.fade)
+        }
+    }
+
+}
